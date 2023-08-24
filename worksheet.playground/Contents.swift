c50f38a67d0d7337a32fe6948ab3be66e6ff1656
Sailaxmi Veldanda <s560208@nwmissouri.edu>
2023-08-24 10:41:47 -0500

    worksheet1

---
+++ worksheet.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var s = ("Hi",10,12.25)
+print(s)
+
+var programmiglanguage = "Swift"
+print("my favourite programming language is \(programmiglanguage)")
+
+var age = 23
+print("You are \(age) years old and in another \(age) years, you will be \(age * 2)")
+
+print("""
+Hello
+World!
+""")
+
+print("hello All, \rWelcome to Swift programming")
+
+let welcomeMessage : String = "Hello"
+print(welcomeMessage,"All")
+
+print("welcome to Swift Programming")
+print("Fall 2023")
+print("************")
+print("Welocme to Swift Programming" , terminator: "-")
+print("Fall 2023")
+
+print("List of Numbers")
+print(1,2,3,4,5,6)
+print("Pattern is")
+print(1,2,3,4,5,6, separator: "-")
