6e76cfe37e9a1338beb109ef537bfdb4146c6440
Sailaxmi Veldanda <s560208@nwmissouri.edu>
2023-08-29 10:44:31 -0500

    done

---
+++ varandconsc.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var mobilebrand = "Apple"
+mobilebrand = "Samsung"
+print(mobilebrand)
+
+let pi = 3.14
+print(pi)
+
+var age : Int = 23
+age = age * 2
+print(age)
+
+var b = "Welcome"
+print(b)
+print("b")
+
+var c1 = "sai"
+var c2 = "laxmi"
+print(c1,c2)
+print(c1,"-",c2)
+
+print(10,20,30)
+print(12.5,15,5)
