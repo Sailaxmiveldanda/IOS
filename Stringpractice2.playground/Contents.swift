4902e03d248af5a77f49e8834a7df4a1ab99c170
Sailaxmi Veldanda <s560208@nwmissouri.edu>
2023-08-31 10:41:36 -0500

    practice2

--- Stringpractice2.playground/Contents.swift
+++ Stringpractice2.playground/Contents.swift
@@ -11,3 +11,33 @@ print("The first item in clothes is socks")
 }else{
 print("socks is not the first item in clothes")
 }
+
+print(foodItems.split(separator: ","))
+
+if clothes.contains(",") {
+print("Clothes contains more than one item")
+}else{
+print("Clothes contain only one item")
+}
+
+foodItems[foodItems.startIndex..<foodItems.index(foodItems.endIndex,offsetBy: -7)]
+
+shoppingList +=
+foodItems[foodItems.index(foodItems.startIndex, offsetBy:
+8)..<foodItems.endIndex]
+
+clothes.remove(at: clothes.firstIndex(of: "T")!)
+
+print(clothes)
+
+clothes.remove(at: clothes.firstIndex(of: "-")!)
+print(clothes)
+
+print("\(shoppingList), \(clothes)")
+
+clothes.insert(contentsOf: ", Trousers", at: clothes.endIndex)
+print(clothes)
+
+var firstIndexOfR = shoppingList.index(after: shoppingList.firstIndex(of: "r")!)
+
+print(shoppingList[..<firstIndexOfR])
