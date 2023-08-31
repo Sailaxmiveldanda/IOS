22d7f90e051696d21f18cc50d7407ae9c49f8c94
Sailaxmi Veldanda <s560208@nwmissouri.edu>
2023-08-31 10:34:55 -0500

    done

---
+++ Stringpractice2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var shoppingList = "The shopping list contains: "
+var foodItems = "Cheese, Butter, Chocolate Spread"
+var clothes = "Socks, T-shirts"
+
+if clothes.hasPrefix("Socks") {
+print("The first item in clothes is socks")
+}else{
+print("socks is not the first item in clothes")
+}
